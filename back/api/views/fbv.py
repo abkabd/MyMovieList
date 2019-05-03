f774e8b6a80a5ab79c482ffd248241f91577fd74
Alibek120699 <sayakalibek1@gmail.com>
2019-05-03 18:31:36 +0600

    2

---
+++ back/api/views/fbv.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+# from api.models import
+# from api.serializers import
+
+@api_view(['GET', 'POST'])
+def get_movies(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
