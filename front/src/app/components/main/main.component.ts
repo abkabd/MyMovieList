97095d9b6f665446dca8e15e9c1840b1eb7f5506
Hafsa Mufassir <hafsamufassir@gmail.com>
2019-05-07 05:12:58 +0600

    a little bit of css

--- front/src/app/components/main/main.component.ts
+++ front/src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProviderService } from '../shared/services/provider.service'
+import { ProviderService} from '../../shared/services/provider.service'
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
