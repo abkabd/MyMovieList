6a260910fd226d6679375c9ff3530329c7d58e1c
Hafsa Mufassir <hafsamufassir@gmail.com>
2019-05-14 15:29:28 +0600

    upd

--- front/src/app/components/footer/footer.component.ts
+++ front/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderService } from 'src/app/shared/services/provider.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private provider: ProviderService) { }
+
 
   ngOnInit() {
   }
